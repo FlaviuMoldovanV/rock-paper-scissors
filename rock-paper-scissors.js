6bc1f4035d0f3e1ae5b1c8fa7b32bcc3e56931c3
FlaviuMoldovanV <69804853+FlaviuMoldovanV@users.noreply.github.com>
2023-10-24 16:53:28 +0300

    feat: playRound() Determines the winner of a round

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,9 +1,43 @@
 "use strict";
 
-//
-function getComputerChoice(){
-    let choices=["rock","paper","scissors"];
-    return choices[Math.floor(Math.random()*choices.length)];
+// Generates random choice for the computer
+function getComputerChoice() {
+    let choices = ["rock", "paper", "scissors"];
+    return choices[Math.floor(Math.random() * choices.length)];
 }
 
-console.log(getComputerChoice());
+//Determines the winner of a round between user and computer
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    console.log("Computer chooses:", computerSelection);
+    if (playerSelection == computerSelection)
+        return "It's a tie!";
+
+    switch (playerSelection) {
+        case "rock":
+            if (computerSelection == "paper")
+                return `You Win! ${playerSelection} beats ${computerSelection}!`;
+            else
+                return `You Lose! ${computerSelection} beats ${playerSelection}!`;
+
+        case "paper":
+            if (computerSelection == "scissors")
+                return `You Win! ${playerSelection} beats ${computerSelection}!`;
+            else
+                return `You Lose! ${computerSelection} beats ${playerSelection}!`;
+
+        case "scissors":
+            if (computerSelection == "rock")
+                return `You Win! ${playerSelection} beats ${computerSelection}!`;
+            else
+                return `You Lose! ${computerSelection} beats ${playerSelection}!`;
+
+        default:
+            return "Please write only the options presented!";
+
+    }
+}
+
+let playerSelection = prompt("Write your choice(rock, paper, scissors)");
+let computerSelection = getComputerChoice();
+alert(playRound(playerSelection, computerSelection));
